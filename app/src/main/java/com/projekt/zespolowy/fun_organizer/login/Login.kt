1a5f52773cdfb8da2130927e9d4f5b4a51b0ba80
Marcin Calkowski <remix11212@e-science.pl>
2018-03-22 21:06:37 +0100

    login activity - changed layout to linear plus data class fix.

--- app/src/main/java/com/projekt/zespolowy/fun_organizer/login/Login.kt
+++ app/src/main/java/com/projekt/zespolowy/fun_organizer/login/Login.kt
@@ -4,7 +4,7 @@ package com.projekt.zespolowy.fun_organizer.login
  * Created by Marcin Całkowski on 22.03.2018.
  * Login class
  */
-class Login(
+data class Login(
         val login: String,
         val password : String
 )
