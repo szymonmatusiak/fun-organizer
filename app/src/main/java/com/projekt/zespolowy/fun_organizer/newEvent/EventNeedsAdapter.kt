a5e0e526d4c968fcaf9847a9cde441aa4b28b685
Marcin Calkowski <remix11212@e-science.pl>
2018-05-09 23:23:03 +0200

    #89 - New event - possibility to delete needed item + some graphical adjustments

--- app/src/main/java/com/projekt/zespolowy/fun_organizer/newEvent/EventNeedsAdapter.kt
+++ app/src/main/java/com/projekt/zespolowy/fun_organizer/newEvent/EventNeedsAdapter.kt
@@ -2,17 +2,16 @@ package com.projekt.zespolowy.fun_organizer.newEvent
 
 import android.support.v7.widget.RecyclerView
 import android.view.ViewGroup
+import java.util.*
 
-class EventNeedsAdapter(private val myDataset: List<EventNeedsModel>):
+class EventNeedsAdapter(private val myDataset: List<EventNeedsModel>, private val itemListener: ItemListener):
         RecyclerView.Adapter<EventNeedsHolder>() {
 
     override fun onCreateViewHolder(viewGroup: ViewGroup, viewType: Int): EventNeedsHolder = EventNeedsHolder.createViewHolder(viewGroup)
 
     override fun onBindViewHolder(holder: EventNeedsHolder, position: Int) {
-        holder.setData(myDataset[position])
+        holder.setData(myDataset[position], itemListener)
     }
-
-
 
     override fun getItemCount() = myDataset.size
 }
